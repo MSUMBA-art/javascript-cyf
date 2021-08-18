5c79d37878de1fab13fc44c6405d81d029fac238
Alexander Msumba <67273919+MSUMBA-art@users.noreply.github.com>
2021-08-18 07:17:42 +0200

    Add files via upload

---
+++ JS REVISION/js1-week1.js
@@ -0,0 +1,8 @@
+// let greetings = "mulibwanji";
+// console.log(greetings);
+// console.log(greetings);
+// console.log(greetings);
+
+const colors = "blue, yellow";
+let color = typeof colors;
+console.log(color);
